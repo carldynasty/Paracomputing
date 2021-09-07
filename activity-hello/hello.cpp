9872db19803e2d8dfe6292c3bdb21d55703d3adc
Louisse Balberan <lbalbera@uncc.edu>
2021-09-06 22:51:18 -0400

    solution

--- activity-hello/hello.cpp
+++ activity-hello/hello.cpp
@@ -1,4 +1,20 @@
-int main () {
+#include <stdio.h>
+#include <limits.h>
+#include <unistd.h>
 
-  return 0;
+int main (){
+
+  int gethostname(char *name, size_t len);
+  int sethostname(const char *name, size_t len);
+  char *getlogin(void);
+  int getlogin_r(char *name, size_t namesize);
+  char hostname[HOST_NAME_MAX];
+  char username[LOGIN_NAME_MAX];
+  gethostname(hostname,HOST_NAME_MAX);
+  getlogin_r(username, LOGIN_NAME_MAX);
+
+  printf("%s\n",gethostname);
+  printf("%s\n",getlogin_r);
+
+    return 0;
 }
